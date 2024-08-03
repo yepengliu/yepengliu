f361dbc9748f6320066cd5d6804fdba388303034
yepengliu <40141652+yepengliu@users.noreply.github.com>
2024-08-03 17:20:37 -0400

    Update avatar.tsx

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -26,7 +26,7 @@ const AvatarImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    className={cn("aspect-4-5 h-full w-full", className)}
     {...props}
   />
 ));
