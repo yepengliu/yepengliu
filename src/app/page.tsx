67f1f3427b85e593898c3cb528811a5d4e509335
yepengliu <40141652+yepengliu@users.noreply.github.com>
2024-08-03 11:39:00 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,10 +79,10 @@ export default function Page() {
             </div>
           </div>
 
-          <Avatar className="size-28">
+{/*           <Avatar className="size-28">
             <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
             <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
-          </Avatar>
+          </Avatar> */}
         </div>
         
         <Section>
