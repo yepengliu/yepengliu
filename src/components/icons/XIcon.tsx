11925a9a452529d29927961f5779f72f471ce132
yepengliu <40141652+yepengliu@users.noreply.github.com>
2024-08-03 17:57:26 -0400

    Update XIcon.tsx

--- src/components/icons/XIcon.tsx
+++ src/components/icons/XIcon.tsx
@@ -3,7 +3,7 @@ export const XIcon = (props: React.SVGProps<SVGSVGElement>) => {
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <title>X</title>
       <path
-        fill="currentColor"
+        fill="#6B7280"
         d="M 213 111 l -107 94 h 69 c 5 45 41 64 78 67 c -7 18 -4 27 7 39 c -43 1 -103 26 -103 67 c 4 45 63 54 92 54 c 38 1 81 -19 90 -54 c 4 -35 -10 -54 -31 -71 c -23 -18 -28 -28 -21 -40 c 15 -17 35 -27 39 -51 c 2 -17 -2 -28 -6 -43 l 45 -38 l -1 16 c -3 2 -5 6 -5 9 v 103 c 2 13 22 11 23 0 V 160 c 0 -3 -2 -7 -5 -8 v -25 l 16 -16 Z m 58 141 c -61 10 -87 -87 -38 -99 c 56 -11 83 86 38 99 Z m -5 73 c 60 13 61 63 10 78 c -44 9 -82 -4 -81 -30 c 0 -25 35 -48 71 -48 Z"
       />
     </svg>
