7c268818dbf5e791b9009ec5bb3c1690d951ab44
yepengliu <40141652+yepengliu@users.noreply.github.com>
2024-08-11 14:51:45 -0400

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -29,7 +29,7 @@ export const RESUME_DATA = {
   email: "yepeng.liu@ufl.edu",
   about: "      ",
   
-  summary: "Hi, I'm Yepeng. I'm currently a second-year Ph.D. student in Electrical and Computer Engineering at the University of Florida. I'm very fortunate to be advised by Prof. Yuheng Bu. My current research focuses on improving the safety, robustness, and reliability of AI models towards their deployment in the real world, especially the Trustworthy Generative Models. Should you be interested in my research or wish to explore collaboration opportunities, please feel free to  reach out to me by email!",
+  summary: "Hi, I'm Yepeng. I'm currently a second-year Ph.D. student in Electrical and Computer Engineering at the University of Florida. I'm very fortunate to be advised by Prof. Yuheng Bu. My current research mainly focuses on improving the safety, robustness, and reliability of AI models towards their deployment in the real world, especially the Trustworthy Generative Models. Should you be interested in my research or wish to explore collaboration opportunities, please feel free to  reach out to me by email!",
   
   // avatarUrl: "https://raw.githubusercontent.com/yepengliu/yepengliu/main/src/app/me.jpg",
   avatarUrl: "https://anonymous.4open.science/api/repo/yepengliu-website-E6CF/file/me.jpg",
