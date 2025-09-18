c5d6664295394da16ffd63bbce2665973adbf0b6
Yepeng Liu <40141652+yepengliu@users.noreply.github.com>
2025-09-18 16:07:25 -0400

    Rename XIcon to GoogleScholar

--- src/components/icons/GoogleScholar.tsx
+++ src/components/icons/GoogleScholar.tsx
@@ -1,4 +1,4 @@
-export const XIcon = (props: React.SVGProps<SVGSVGElement>) => {
+export const GoogleScholar = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" {...props}>
       <title>X</title>
