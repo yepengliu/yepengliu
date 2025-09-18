8e54d57e30db4436eb8fc82357891bc727b2c8e5
Yepeng Liu <40141652+yepengliu@users.noreply.github.com>
2025-09-18 16:54:22 -0400

    Refactor X icon component and update SVG structure

--- src/components/icons/X.tsx
+++ src/components/icons/X.tsx
@@ -1,41 +1,49 @@
-import * as React from "react"
-import { SVGProps } from "react"
+import * as React from "react";
 
-const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+export const CustomIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
     viewBox="0 0 256 256"
-    width={48}
-    height={48}
-    fillRule="none"
+    width="48px"
+    height="48px"
     {...props}
   >
     <g
       fill="none"
-      strokeMiterlimit={10}
+      stroke="none"
+      strokeWidth="1"
+      strokeLinecap="butt"
+      strokeLinejoin="miter"
+      strokeMiterlimit="10"
       fontFamily="none"
       fontWeight="none"
       fontSize="none"
       textAnchor="none"
-      style={{
-        mixBlendMode: "normal",
-      }}
+      style={{ mixBlendMode: "normal" }}
     >
-      <path
-        d="M202.667 224H53.333C41.552 224 32 214.448 32 202.667V53.333C32 41.552 41.552 32 53.333 32h149.334C214.447 32 224 41.552 224 53.333v149.334c0 11.78-9.552 21.333-21.333 21.333z"
-        fill="#6b7280"
-        fillRule="evenodd"
-      />
-      <path
-        d="M182.704 181.333h-34.33L73.753 74.667h34.331zm-30.24-9.045h13.67l-62.14-88.576H90.326z"
-        fill="#fff"
-        fillRule="nonzero"
-      />
-      <path
-        d="m84.619 181.333 38.416-44.501-5.024-6.661-44.288 51.162zM130.4 115.845l4.827 6.875 41.498-48.053H166.06z"
-        fill="#fff"
-        fillRule="nonzero"
-      />
+      <g transform="scale(5.33333,5.33333)">
+        <path
+          d="M38,42h-28c-2.209,0 -4,-1.791 -4,-4v-28c0,-2.209 1.791,-4 4,-4h28c2.209,0 4,1.791 4,4v28c0,2.209 -1.791,4 -4,4z"
+          fill="#6b7280"
+          fillRule="evenodd"
+        />
+        <path
+          d="M34.257,34h-6.437l-13.991,-20h6.437zM28.587,32.304h2.563l-11.651,-16.608h-2.563z"
+          fill="#ffffff"
+          fillRule="nonzero"
+        />
+        <path
+          d="M15.866,34l7.203,-8.344l-0.942,-1.249l-8.304,9.593z"
+          fill="#ffffff"
+          fillRule="nonzero"
+        />
+        <path
+          d="M24.45,21.721l0.905,1.289l7.781,-9.01h-2z"
+          fill="#ffffff"
+          fillRule="nonzero"
+        />
+      </g>
     </g>
   </svg>
-)
+);
