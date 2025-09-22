6d566c3d84fa926c76c9c5abab272513fac37d8f
Yepeng Liu <40141652+yepengliu@users.noreply.github.com>
2025-09-22 15:30:47 -0400

    Change location and contact information

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -24,12 +24,12 @@ export const RESUME_DATA = {
     
   name: "Yepeng Liu",
   initials: "YL",
-  location: "Gainesville, Florida, USA",
-  locationLink: "https://maps.app.goo.gl/TjqGZppaxcaNvu5r9",
-  email: "yepeng.liu@ufl.edu",
+  location: "Santa Barbara, California, USA",
+  locationLink: "https://maps.app.goo.gl/PzanfcBVWB6FHqnX6",
+  email: "yepengliu@ucsb.edu",
   about: "      ",
   
-  summary: "Hi, I'm Yepeng. I'm currently a second-year Ph.D. student in Electrical and Computer Engineering at the University of Florida. I'm very fortunate to be advised by Prof. Yuheng Bu. My current research mainly focuses on improving the safety, robustness, and reliability of AI models towards their deployment in the real world, especially the Trustworthy Generative Models. Should you be interested in my research or wish to explore collaboration opportunities, please don't hesitate to drop me an email!",
+  summary: " ",
 
   avatarUrl: "https://raw.githubusercontent.com/yepengliu/yepengliu/main/me.jpg",
   // avatarUrl: "https://anonymous.4open.science/api/repo/yepengliu-website-E6CF/file/me.jpg",
